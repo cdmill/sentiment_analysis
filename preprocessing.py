5130c1d6247ed0a03f91960e37de120e067fa5ea
Casey Miller <cd.miller1230@gmail.com>
2023-05-16 19:36:04 -0600

    refactored, randomized, added output

--- preprocessing.py
+++ preprocessing.py
@@ -2,12 +2,11 @@
 twitter corpus from the nltk module
 """
 
-import nltk
-from nltk.corpus import twitter_samples
-import matplotlib.pyplot as plt
-import random
 import re
 import string
+import nltk
+import random
+from nltk.corpus import twitter_samples
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
@@ -18,30 +17,33 @@ nltk.download("stopwords")
 positive_tweets = twitter_samples.strings("positive_tweets.json")
 negative_tweets = twitter_samples.strings("negative_tweets.json")
 
-fig = plt.figure(figsize=(5, 5))
-labels = "Positives", "Negatives"
-sizes = [len(positive_tweets), len(negative_tweets)]
+size = len(positive_tweets)
+tweet = positive_tweets[random.randrange(size)]
+print("original:", tweet, sep='\n')
+print('\n')
 
-plt.pie(sizes, labels=labels, autopct="%1.1f%%", shadow=True, startangle=90)
-plt.axis("equal")
-plt.show()
-
-# remove punctuation, links, etc from tweets
-tweet = positive_tweets[2277]
+# remove hyperlinks, mentions, and hashtags from tweets
 tweet_modified = re.sub(r'^RT[\s]+', '', tweet)
 tweet_modified = re.sub(r'https?://[^\s\n\r]+', '', tweet_modified)
 tweet_modified = re.sub(r'#', '', tweet_modified)
+print("hyperlinks, mentions, and hashtags removed:", tweet_modified, sep='\n')
+print('\n')
 
+# tokenize
 tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 tweet_tokens = tokenizer.tokenize(tweet_modified)
-
-stopwords_english = stopwords.words("english")
+print("tokenized:", tweet_tokens, sep='\n')
+print('\n')
 
 # remove stop words from tweets
+stopwords_english = stopwords.words("english")
 tweets_clean = []
 for word in tweet_tokens:
     if word not in stopwords_english and word not in string.punctuation:
         tweets_clean.append(word)
+
+print("stop words removed:", tweets_clean, sep='\n')
+print('\n')
 
 # stem the tweets
 stemmer = PorterStemmer()
@@ -50,5 +52,6 @@ for word in tweets_clean:
     stem_word = stemmer.stem(word)
     tweets_stem.append(stem_word)
 
-print(tweets_stem)
+print("stemmed:", tweets_stem, sep='\n')
+print('\n')
 
